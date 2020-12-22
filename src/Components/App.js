690eb70996ded8d01a5eb9991231b0db56fb920b
irina-mishchenko <i.mishchenko95@gmail.com>
2020-12-23 00:38:40 +0200

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,78 @@
+import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const identicalName = contacts.find(contact => {
+      return contact.name.toLowerCase() === name.toLowerCase();
+    });
+
+    if (identicalName) {
+      alert(`${name} is already in contacts!`);
+      return;
+    }
+
+    const contact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  changeFilter = ({ target }) => {
+    const filter = target.value;
+    this.setState({ filter });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId,
+        ),
+      };
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <div>
+        <ContactForm onAddContact={this.addContact} />
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={this.getVisibleContacts()}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,25 @@
+import propTypes from 'prop-types';
+
+import s from './Filter.module.css';
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <div className={s.filterContainer}>
+      <label className={s.filterLabel}>
+        Find contact
+        <input
+          className={s.filterInput}
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  value: propTypes.string.isRequired,
+};
+
+export default Filter;
